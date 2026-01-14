778da50745e7ab310383e4b7e6a990a1c3355558
Ryhandd <rayhandwumu6@gmail.com>
2026-01-14 23:22:54 +0800

    Fix all remaining syntax errors and conflicts

--- database/chessHandler.js
+++ database/chessHandler.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { makeMove } = require('./commands/chessGame.js')
 
 module.exports = async function chessHandler({ from, sender, text, reply }) {
@@ -11,17 +10,3 @@ module.exports = async function chessHandler({ from, sender, text, reply }) {
   reply(res)
   return true
 }
-=======
-const { makeMove } = require('./commands/chessGame.js')
-
-module.exports = async function chessHandler({ from, sender, text, reply }) {
-  const game = global.chessGames[from]
-  if (!game) return false
-
-  if (!/^(o-o|o-o-o|[nbrqk]?[a-h][1-8])$/i.test(text)) return false
-
-  const res = makeMove(from, sender, text)
-  reply(res)
-  return true
-}
->>>>>>> 867da6c2ae86083a8435459a145ae4f01677e69d
