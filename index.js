d878be507d367962f39c80b9cfc0e3fba8eb7e09
Ryhandd <rayhandwumu6@gmail.com>
2026-01-28 08:49:31 +0800

    Update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import http from "http"
 http.createServer((req, res) => res.end("Ryzu Bot is Online!"))
   .listen(process.env.PORT || 8080)
-import qrcode from "qrcode-terminal"
+//import qrcode from "qrcode-terminal"
 import "dotenv/config"
 import { webcrypto } from "crypto"
 globalThis.crypto = webcrypto
