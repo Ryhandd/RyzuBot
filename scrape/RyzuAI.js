82f339ca054e89db8f7ec059b36aff236f955cf8
Ryhandd <rayhandwumu6@gmail.com>
2026-03-30 10:51:07 +0800

    Update

--- scrape/RyzuAI.js
+++ scrape/RyzuAI.js
@@ -8,7 +8,7 @@ async function RyzuAI(prompt) {
         const res = await axios.post(
             "https://api.groq.com/openai/v1/chat/completions",
             {
-                model: "llama3-70b-8192",
+                model: "llama-3.3-70b-versatile",
                 messages: [{ role: "user", content: prompt }]
             },
             {
