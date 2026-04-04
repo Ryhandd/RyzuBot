8d988a18d1002d3a2e4b40b48b42220e5d22f496
Ryhandd <rayhandwumu6@gmail.com>
2026-04-05 00:07:47 +0800

    Update

--- database/commands/sticker.js
+++ database/commands/sticker.js
@@ -260,7 +260,7 @@ module.exports = {
 
         fs.writeFileSync(input, buffer)
 
-        execSync(`ffmpeg -y -i "${input}" "${output}"`)
+        execSync(`ffmpeg -y -ignore_loop 0 -i "${input}" -movflags faststart -pix_fmt yuv420p -vf "fps=15,scale=512:512:force_original_aspect_ratio=increase,crop=512:512" "${output}"`)
 
         const result = fs.readFileSync(output)
 
@@ -293,9 +293,7 @@ module.exports = {
 
         fs.writeFileSync(input, buffer)
 
-        execSync(
-          `ffmpeg -y -i "${input}" -movflags faststart -pix_fmt yuv420p -vf "scale=512:512:force_original_aspect_ratio=increase,crop=512:512" "${output}"`
-        )
+        execSync(`ffmpeg -y -ignore_loop 0 -i "${input}" -movflags faststart -pix_fmt yuv420p -vf "fps=15,scale=512:512:force_original_aspect_ratio=increase,crop=512:512" "${output}"`)
 
         const result = fs.readFileSync(output)
 
