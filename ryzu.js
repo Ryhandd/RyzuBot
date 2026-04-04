9c2a8bef750aa4c5a56441a0b6669d7abd5f6595
Ryhandd <rayhandwumu6@gmail.com>
2026-04-04 12:56:04 +0800

    Update

--- ryzu.js
+++ ryzu.js
@@ -558,7 +558,7 @@ module.exports = async function ryzuHandler(ryzu, m) {
 
                 if (isTerjawab && penjawab) {
                     mentions.push(penjawab);
-                    teks += `${i + 1}. ${j} ✅ (@${penjawab.split("@")})\n`;
+                    teks += `${i + 1}. ${j} ✅ (@${penjawab.split("@")[0]})\n`;
                   } else teks += `${i + 1}. ??\n`;
               });
               teks += `\n🎁 +5000 Money | +500 EXP${up ? "\n🎊 LEVEL UP!" : ""}`;
